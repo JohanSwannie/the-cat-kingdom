5c4b4c86889aa19b43c8df97694d8c3094061481
jswan <jswannieman@gmail.com>
2023-02-07 16:29:07 +1100

    Update Configurations

---
+++ src/components/card/card.functional-component.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Cats } from '../../AppFunction';
+import './card.styles.css';
+
+type CardProps = {
+    cat: Cats;
+  };
+
+const Card = ({cat}: CardProps) => {
+    const { id, name, email } = cat;
+    return (
+        <div className="card-container" key={id}>
+            <img 
+                src={`https://robohash.org/${id}?set=set4&size=180x180`} 
+                alt={`cat ${name}`}/>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+
+}
+
+export default Card;
