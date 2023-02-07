097f4344c5fba532af71f1be1f4f4b7bf4c5f9fe
jswan <jswannieman@gmail.com>
2023-02-07 16:32:10 +1100

    Update the card-list tsx Component

---
+++ src/components/card-list/card-list.functional-component.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '../card/card.functional-component';
+import { Cats } from '../../AppFunction';
+import './card-list.styles.css';
+
+type CardListProps = {
+  cats: Cats[];
+};
+
+const CardList = ({ cats }: CardListProps) => (
+        <div className="card-list">
+            {cats.map(cat => {
+                return ( 
+                    <Card cat={cat} />
+                )
+            })}
+        </div>
+);
+
+export default CardList;
